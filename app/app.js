361a66d186171532d2ada2aedaa5841503b3663c
Rakesh Patel <emailme@rakeshrpatel.com>
2016-06-09 11:15:03 -0400

    successfully grabbed data

--- app/app.js
+++ app/app.js
@@ -1,3 +1,32 @@
-angular.module('calendarDemoApp', []);
+var calendarDemoApp = angular.module('calendarDemoApp', [])
+
+    .controller('dataCompiler', ['$scope', function($scope) {
+
+        $scope.monthsSelector = [
+        'January', 'February', 'March', 
+        'April', 'May', 'June', 
+        'July', 'August', 'September', 
+        'October', 'November', 'December'
+        ];
+
+        var d = new Date();
+        $scope.monthSelected = $scope.monthsSelector[d.getMonth()];
+        $scope.yearSelected = d.getFullYear();
+        
+        $scope.submission = function(){
+            console.log($scope.monthSelected.toString(), $scope.yearSelected.toString())
+            console.log(CalendarRange.getMonthlyRange($scope.monthSelected.toString() + $scope.yearSelected.toString()));
+        }
+
+        $scope.years = [];
+        	for(i=((CalendarRange.prepareDate(Date.now()).year) - 20); i <= (CalendarRange.prepareDate(Date.now()).year); i++) {
+        		$scope.years.push(i);
+        	}
+        	
+        	for(i=CalendarRange.prepareDate(Date.now()).year; i <= ((CalendarRange.prepareDate(Date.now()).year) + 20); i++) {
+        		$scope.years.push(i);
+        	}
+
+    }])
 
 // your controller and directive code go here
